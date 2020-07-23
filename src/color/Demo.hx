4b75551ffb1e79b72be878c9379b9de21f51e25c
Tom <tjpalmer@tjpalmer.com>
2020-07-23 07:34:34 -0700

    Maybe finish demo

--- src/color/Demo.hx
+++ src/color/Demo.hx
@@ -1,5 +1,7 @@
 package color;
 
+using color.Tools;
+
 enum Color {
   Rgb(r:Float, g:Float, b:Float);
   Hsv(h:Float, s:Float, v:Float);
@@ -23,11 +25,22 @@ class Demo {
   }
 
   static public function hsvToRgb(h:Float, s:Float, v:Float):Color {
-    return Rgb(h, s, v);
+    var c = s * v;
+    var h1 = h * 6;
+    var x = c * (1 - Math.abs(h1 % 2 - 1));
+    var rgb1 =
+      if (h1 < 1)  Rgb(c, x, 0);
+      else if (h1 < 2) Rgb(x, c, 0);
+      else if (h1 < 3) Rgb(0, c, x);
+      else if (h1 < 4) Rgb(0, x, c);
+      else if (h1 < 5) Rgb(x, 0, c);
+      else Rgb(c, 0, x);
+    var m = v - c;
+    var rgb = rgb1.extract(Rgb(r, g, b) => Rgb(r + m, g + m, b + m));
+    return rgb;
   }
 
   static public function main():Void {
-    var named = Named("red");
     var colors = [Hsv(1, 1, 1), Named("yellow"), Rgb(0, 0, 1)];
     for (index => color in colors) {
       trace('Color ${index}: ${colorToRgb(color)}');
---
+++ src/color/Tools.hx
@@ -0,0 +1,21 @@
+package color;
+
+// From: https://code.haxe.org/category/macros/extract-enum-value.html
+
+#if macro
+import haxe.macro.Expr;
+#end
+
+class Tools {
+  public static macro function extract(value:ExprOf<EnumValue>, pattern:Expr):Expr {
+    switch (pattern) {
+      case macro $a => $b:
+        return macro switch ($value) {
+          case $a: $b;
+          default: throw "no match";
+        }
+      default:
+        throw new Error("Invalid enum value extraction pattern", pattern.pos);
+    }
+  }
+}
