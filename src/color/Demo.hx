d6216a6a4a7d5f03bed5a3bd1d4af7269c016682
Tom <tjpalmer@tjpalmer.com>
2020-07-23 06:36:20 -0700

    Explore alternatives

--- src/color/Demo.hx
+++ src/color/Demo.hx
@@ -1,23 +1,45 @@
 package color;
 
+class Hsv {
+  public var h:Float;
+  public var s:Float;
+  public var v:Float;
+  public function new(h:Float, s:Float, v:Float) {
+    this.h = h;
+    this.s = s;
+    this.v = v;
+  }
+}
+
+// typedef Hsv = {h:Float, s:Float, v:Float};
+typedef Rgb = {r:Float, g:Float, b:Float};
+
 enum Color {
-  Rgb(r:Float, g:Float, b:Float);
-  Hsv(h:Float, s:Float, v:Float);
+  Rgb(rgb:Rgb);
+  Hsv(hsv:Hsv);
   Named(name:String);
 }
 
 class Demo {
 
-  static public function toRgb(color:Color): Color {
+  static var namedColors:Map<String, Rgb> = [
+    "red" => {r: 1, g: 0, b: 0},
+    "yellow" => {r: 1, g: 1, b: 0},
+    "blue" => {r: 0, g: 0, b: 1},
+  ];
+
+  static public function colorToRgb(color:Color):Rgb {
     return switch (color) {
-      case Rgb(_): color;
-      case Hsv(h, s, v): color;
-      case Named(name): color;
+      case Rgb(rgb): rgb;
+      case Hsv(_): {r: 1, g: 1, b: 1};
+      case Named(name): {r: 1, g: 1, b: 1};
     }
   }
 
   static public function main():Void {
     var named = Named("red");
+    var hi = new Hsv(1, 1, 1);
+    // var hi: Hsv = {h: 1, s: 1, v: 1};
     trace("Hi there!");
   }
 
