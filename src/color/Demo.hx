e45b006ae4e76226e396ae3f38e2227ac693572a
Tom <tjpalmer@tjpalmer.com>
2020-07-23 07:08:20 -0700

    Just use enums

--- src/color/Demo.hx
+++ src/color/Demo.hx
@@ -1,46 +1,37 @@
 package color;
 
-class Hsv {
-  public var h:Float;
-  public var s:Float;
-  public var v:Float;
-  public function new(h:Float, s:Float, v:Float) {
-    this.h = h;
-    this.s = s;
-    this.v = v;
-  }
-}
-
-// typedef Hsv = {h:Float, s:Float, v:Float};
-typedef Rgb = {r:Float, g:Float, b:Float};
-
 enum Color {
-  Rgb(rgb:Rgb);
-  Hsv(hsv:Hsv);
+  Rgb(r:Float, g:Float, b:Float);
+  Hsv(h:Float, s:Float, v:Float);
   Named(name:String);
 }
 
 class Demo {
 
-  static var namedColors:Map<String, Rgb> = [
-    "red" => {r: 1, g: 0, b: 0},
-    "yellow" => {r: 1, g: 1, b: 0},
-    "blue" => {r: 0, g: 0, b: 1},
+  static var namedColors: Map<String, Color> = [
+    "red" => Rgb(1, 0, 0),
+    "yellow" => Rgb(1, 1, 0),
+    "blue" => Rgb(0, 0, 1),
   ];
 
-  static public function colorToRgb(color:Color):Rgb {
+  static public function colorToRgb(color:Color):Color {
     return switch (color) {
-      case Rgb(rgb): rgb;
-      case Hsv(_): {r: 1, g: 1, b: 1};
-      case Named(name): {r: 1, g: 1, b: 1};
+      case Rgb(_): color;
+      case Hsv(h, s, v): hsvToRgb(h, s, v);
+      case Named(name): namedColors[name];
     }
+  }
+
+  static public function hsvToRgb(h:Float, s:Float, v:Float):Color {
+    return Rgb(h, s, v);
   }
 
   static public function main():Void {
     var named = Named("red");
-    var hi = new Hsv(1, 1, 1);
-    // var hi: Hsv = {h: 1, s: 1, v: 1};
-    trace("Hi there!");
+    var colors = [Hsv(1, 1, 1), Named("yellow"), Rgb(0, 0, 1)];
+    for (index => color in colors) {
+      trace('Color ${index}: ${colorToRgb(color)}');
+    }
   }
 
 }
